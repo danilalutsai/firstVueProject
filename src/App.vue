f392cb753541a1b7c827fbc998b02c83311d5747
Danila Lutsai <dlutsai98@gmail.com>
2026-07-24 21:50:42 +0300

    Vue TS basics

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,72 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive, ref } from 'vue'
+import type Job from './types/Job'
+
+// With ref() we use <generic type> argument instead of type assertion : type
+const message = ref<string>('Hello, hyrule');
+const name = ref<string>('Link');
+
+// We don't pass ref() because is a value that we will never change or use interactively
+// With ref() wraps any value (primitives and objects). You access it via .value in the script
+const age = ref<number>(25);
+const count = ref<number>(0);
+
+// With reactive() wraps only objects/arrays (not primitives). No .value, you use it directly
+// If you access variable with reactive() you access properties not wrapped -> object.property (syntax)
+const state = reactive({ count: 0 as number, name: 'Josh' as string });
+
+// Passing the types in the object with -> as type-name
+const object = {
+  name: "Danila" as string,
+  age: 25 as number,
+}
+
+const jobs = ref<Job[]>([{ id: 'id1', title: 'machinist', location: 'Tallinn', salary: 500 }]);
+
+// Using composition API from -> ref() -> .value
+function add() {
+  count.value++
+  console.log(count.value);
+}
+
+function modifyState() {
+  state.count++;
+  state.name = 'Danila';
+}
+
+function changeAge(newAge: number) {
+  age.value = newAge;
+}
+
+function changeName(newName: string) {
+  name.value = newName;
+}
+
+console.log(count.value);
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
   </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <main>
+      <div class="app">
+
+      <p>His name is {{ name }} and is {{ age }} years old and the count is {{ count }}.</p>
+
+      <p>First worker is {{ jobs[0]?.title }}.</p>
+
+      <button @click="changeName('Danila')">Change name</button><br><br>
+      <button @click="changeAge(42)">Change age</button><br><br>
+      <button @click="add">Count: {{ count }}</button><br><br>
+      <button @click="modifyState">Count + and change name</button><br><br>
+
+      <!-- With the reactive keyword you have to access the data with full name-of-object.name-of-property -->
+      <p>Name is {{ state.count }} and name is {{ state.name }}.</p>
+
+      </div>
+    </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
